130e32f441fd4268a5c9827ae08d9fafdf2f981d
axiomcura <erikishere3@gmail.com>
2021-04-03 13:18:59 -0700

    added new program

---
+++ scripts/checktraj.py
@@ -0,0 +1,17 @@
+import os 
+import sys
+import argparse 
+
+
+def parse_outfile():
+    """ parses the amber output file and extract the data"""
+    pass
+
+def formatter():
+    """ formats the data on how it is """
+
+if __name__ == '__main__':
+    pass
+    # CLI 
+
+    # main code blow
